1f5200c77b246fb7acdbbcf55d753d4529c96e7d
weihuichao <505403397@qq.com>
2016-05-11 12:41:39 +0800

    https://github.com/weihuichao/weihuichao.git

---
+++ java script/hello.js
@@ -0,0 +1,27 @@
+// alert("Hi");
+// dfdfdf
+// dfdfd
+// dfdfd
+// var 性别 = '女';
+// alert(性别);
+var age = 90;
+// alert(20 == '20');
+// {
+// alert(age + 3);
+// alert("13" + "广告");
+// }
+if (age > 40) {
+if (age >= 90) { alert("天天向上"); } else {
+alert("好好学习");
+}
+} else {
+alert("广告");
+}
+for (var i = 0; i < 100; i++) {
+console.log(i)
+// 在控制台输出信息
+}
+function sayHello(name) {
+console.log(name + ", 你好");
+}
+sayHello("杨老师");
